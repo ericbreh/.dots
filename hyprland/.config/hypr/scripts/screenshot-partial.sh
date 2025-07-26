e3d2c1b6b98307f5644e7c779f6633bc10ff5ce8
Eric Chuang <ericchuang94@gmail.com>
2025-07-25 20:54:25 -0700

    fix screenshot

--- hyprland/.config/hypr/scripts/screenshot-partial.sh
+++ hyprland/.config/hypr/scripts/screenshot-partial.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-if area=$(slurp); then grim -g "$area" - | tee >(wl-copy) >~/Pictures/Screenshots/Screenshot-"$(date +%F_%T)".png && notify-send "Screenshot of the region taken"; fi
+if area=$(slurp); then mkdir -p ~/Pictures/Screenshots && grim -g "$area" - | tee >(wl-copy) >~/Pictures/Screenshots/Screenshot-"$(date +%F_%T)".png && notify-send "Screenshot of the region taken"; fi
