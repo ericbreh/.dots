57f55ee6b6db2f18cdfd64f6f072c4497c2c4dcd
Eric Chuang <ericchuang94@gmail.com>
2025-11-20 14:00:27 -0800

    flutter and pandoc nvim

--- nvim/.config/nvim/lua/custom/plugins/markdown.lua
+++ nvim/.config/nvim/lua/custom/plugins/markdown.lua
@@ -8,5 +8,15 @@ return {
   ft = { 'markdown' },
   keys = {
     { '<leader>km', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Toggle[K] [M]arkdown Preview' },
+    {
+      '<leader>kp',
+      function()
+        local current_file = vim.fn.expand('%')
+        local pdf_file = vim.fn.expand('%:r') .. '.pdf'
+        local command = '!pandoc ' .. current_file .. ' -o ' .. pdf_file .. ' && xdg-open ' .. pdf_file
+        vim.cmd(command)
+      end,
+      desc = 'Convert to PDF using Pandoc',
+    },
   },
 }
